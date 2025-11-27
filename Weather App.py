c2f6ecc50c6d570df49bc23afe5973c329d0f3ba
Nathaniel Olguin <business.nateolguin@gmail.com>
2025-11-26 23:05:07 -0600

    Weather App using API | Final update
    Weather App using API
    
    This project uses an API to get weather based off users zip code which then takes the latitude and longitude based off of the zip-code and uses them to get the exact location and display basic real-time weather statistics. 
    
    Completed in this commit:
       -   Completed the project***
       -   Increased my understanding of how to use the 'requests' module
       -   Increased my understanding of how to use an API
       -   Increased my understanding of the HTTPS status codes
       -   Fixed the dictionary key/value issue that was present in the prior commit, 33a5afc.
    
    
    NOTES:
       -   I attempted to use the original API from my initial commit, the direct weather API, and the zip-code API giving the user the option to choose which one to use. However, the direct did not provide accurate results. That API was scrapped and replaced with solely the zip-code API which provided the lat and lon required to get the exact locations live weather.

--- Weather App.py
+++ Weather App.py
@@ -24,44 +24,74 @@ def space(x=0):
             
             
 #Project User-Functions
-def get_weather(city='Abilene'):
-    url = f'{weather_url}q={city}&limit=5&appid={my_api_key}'
+def get_location(zip):
+    url = f'{location_url_zip}zip={zip}&appid={my_api_key}'    #link
     http = requests.get(url)    #https status code
-    print(f'HTTPS status code: {http}')
-    
+    print(f'Location HTTPS status code: {http}')
     if http.status_code == 200:    #method that reads the status code as int from requests module
         print('Success!')
-        weather_data = http.json()
-        return weather_data
+        location_data = http.json()    #new variable assigned to the API results
+        return location_data
+    else:
+        print(f'The server: {http} failed to return a successful value.')
+
+
+def get_forcast(lat, lon):
+    url = f'{forcast_url}lat={lat}&lon={lon}&appid={my_api_key}&units=imperial'    #link
+    http = requests.get(url)    #https status code
+    print(f'Forcast HTTPS status code: {http}')
+    if http.status_code == 200:    #method that reads the status code as int from requests module
+        print('Success!')
+        forcast_data = http.json()    #new variable assigned to the API results
+        return forcast_data
     else:
         print(f'The server: {http} failed to return a successful value.')
 
 
 #VARIABLES
-weather_url = 'http://api.openweathermap.org/geo/1.0/direct?'
+location_url_zip = 'http://api.openweathermap.org/geo/1.0/zip?'
 my_api_key = 'f7d10bb35e39017b227c6fa95ab29064'
-# = https://api.weather.gov/
+forcast_url = 'https://api.openweathermap.org/data/2.5/weather?'
 
 
-#STEP 1 - collecting user location
+#***STEP 1 - prompting user for location via zip***
 print('Welcome to the Python Weather app')
 space()
-city = input('Please enter your city: ').capitalize().strip()
+try:
+    zip = int(input('Please enter you zip code: '))
+except Exception:
+    space()
+    print('***Invalid entry: default zip set to: 79601')
+    zip = 79601
 space('line')
+location_info = get_location(zip)
 
 
-#STEP 2 - returning user location
-get_weather(city)
-weather_info = get_weather(city)
-
-print(weather_info)    #check dictionary values
+#***STEP 2 - use variable: 'location_info' latitude and longitude to get forcast***
 space()
+forcast_info = get_forcast(location_info["lat"], location_info["lon"])
 
 
-#STEP 3 - 
-if weather_info:
-    print(f'Name: {weather_info[name].capitalize()}')
-    print(f'Country: {weather_info[country].capitalize()}')
-    print(f'State: {weather_info[state].capitalize()}')
+#***STEP 3 - returning user location***
+space('line')
+#print(location_info)    #used to get the dictionary names to call when developing this ptoject
+if location_info:
+    print(f'        Name       :  {location_info["name"]}')
+    print(f'        Country    :  {location_info["country"]}')
+    print(f'        Latitude   :  {location_info["lat"]}')
+    print(f'        Longitude  :  {location_info["lon"]}')
+space('line')
+#print(forcast_info)    #used to get the dictionary names to call when developing this ptoject
+if forcast_info:
+    print(f'        Temp       :  {forcast_info["main"]["temp"]}°F')
+    print(f'        feels like :  {forcast_info["main"]["feels_like"]}°F')
+    print(f'        Max Temp   :  {forcast_info["main"]["temp_max"]}°F')
+    print(f'        Min Temp   :  {forcast_info["main"]["temp_min"]}°F')
+    print(f'        Weather    :  {forcast_info["weather"][0]["description"]}')
 
 
+
+
+
+
+    
