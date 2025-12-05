607c7c42279a5e327376206784bb2acc06a91bbb
Nathaniel Olguin <business.nateolguin@gmail.com>
2025-12-05 07:58:19 -0600

    Finish: Image File Organizer
    NOTES:
       -   TO USE: run the program in the desired directory that you want to organize image file types within. For me, I ran in it in my downloads folder since I very rarely place downloaded files in a designated directory on my own. Using this program cleaned up my downloads directory since I had several of the files in it scattered around prior to running the program. 
       -   THE GOAL: to use the initial_directory (the directory the program is located in) to then scrub through the files and identify file types (image file types) and add the desired file types (image file types) into the designated directories
       -   For this project I studied the OS & the SHUTIL modules within python so I could achieve my first automated task with a python program. 
    
    
    COMPLETED IN THIS COMMIT:
       -   Increased knowledge about the OS module
       -   Learned about the SHUTIL module and how to use it alongside the OS module
       -   Finish the project***
       -   Finish my 1st program for automating tasks with Python

--- Image File Organizer.py
+++ Image File Organizer.py
@@ -2,22 +2,24 @@
 #    -    Import the os module.
 #    -    Move files into folders by file type (e.g., .jpg → “Images”).
 #SKILLS:    This uses loops, conditionals, imports, and practical applications
-#NOTES:    decided to make this for image file types specifically. 
+#NOTES:    decided to make this for image file types specifically. When finishing this project I initally thought that the OS module was all that was needed for this project but while researching I discoved that the SHUTIL module would be perfect for completing this project
 
 
 #IMPORTS
 import os
+import shutil
 
 
 #STEP 1 - Obtain starting directory and assign to variable
 initial_directory = os.getcwd()
 initial_directory_list = os.listdir(os.getcwd())
-print(initial_directory)
+print(initial_directory)    #for reference
 
 
 #STEP 2 - Creating image file type folders
 os.makedirs('Organized Images\\PNG', exist_ok=True)
-image_types = os.chdir('Organized Images')    #update current directory to the newly created folder 'Organized Images'
+os.chdir('Organized Images')    #update initial directory to the newly created folder 'Organized Images'
+new_directory = os.getcwd()
 os.makedirs('JPEG', exist_ok=True)
 os.makedirs('SVG', exist_ok=True)
 os.makedirs('GIF', exist_ok=True)
@@ -43,18 +45,61 @@ while len(initial_directory_list) > 0:
     if os.path.exists(file_name):    #verifying the file exists
         print(True)
         print(f'File Size:  {os.stat(file_name).st_size}')
-        #verify image file type
-        if '.png' in file_name:
+        #verify image file type then move the file types into the new_directory plus the designated file type folder
+        if '.png' in file_name or '.PNG' in file_name:
             print('File Type:  PNG')
-        elif '.jpg' in file_name:
+            shutil.move(file_name, f'{new_directory}\\PNG')
+        elif '.jpg' in file_name or '.JPG' in file_name:
             print('File Type:  JPG')
-            
+            shutil.move(file_name, f'{new_directory}\\JPEG')
+        elif '.svg' in file_name or '.SVG' in file_name:
+            print('File Type:  SVG')
+            shutil.move(file_name, f'{new_directory}\\SVG')
+        elif '.gif' in file_name or '.GIF' in file_name:
+            print('File Type:  GIF')
+            shutil.move(file_name, f'{new_directory}\\GIF')
+        elif '.webp' in file_name or '.WEBP' in file_name:
+            print('File Type:  WebP')
+            shutil.move(file_name, f'{new_directory}\\WebP')
+        elif '.heif' in file_name or '.HEIF' in file_name:
+            print('File Type:  HEIF')
+            shutil.move(file_name, f'{new_directory}\\HEIF')
+        elif '.tiff' in file_name or '.TIFF' in file_name:
+            print('File Type:  TIFF')
+            shutil.move(file_name, f'{new_directory}\\TIFF')
+        elif '.bmp' in file_name or '.BMP' in file_name:
+            print('File Type:  BMP')
+            shutil.move(file_name, f'{new_directory}\\BMP')
+        elif '.psd' in file_name or '.PSD' in file_name:
+            print('File Type:  PSD')
+            shutil.move(file_name, f'{new_directory}\\PSD')
+        elif '.pdf' in file_name or '.PDF' in file_name:
+            print('File Type:  PDF')
+            shutil.move(file_name, f'{new_directory}\\PDF')
+        elif '.raw' in file_name or '.RAW' in file_name:
+            print('File Type:  RAW')
+            shutil.move(file_name, f'{new_directory}\\RAW')
+        elif '.eps' in file_name or '.EPS' in file_name:
+            print('File Type:  EPS')
+            shutil.move(file_name, f'{new_directory}\\EPS')
+        elif '.ai' in file_name or '.AI' in file_name:
+            print('File Type:  AI')
+            shutil.move(file_name, f'{new_directory}\\AI')
+        elif '.indd' in file_name or '.INDD' in file_name:
+            print('File Type:  INDD')
+            shutil.move(file_name, f'{new_directory}\\INDD')
+        elif '.dwg' in file_name or '.DWG' in file_name:
+            print('File Type:  DWG')
+            shutil.move(file_name, f'{new_directory}\\DWG')
+        else:
+            print('File Type:  NOT AN IMAGE TYPE')
+
     else:
-        print('File Type:  ***Ignored by this program (only looking for image file types)')
+        print('File not found')    #verifying the file exists
     
 
-#STEP 4 - NOT FINISHED YET
+#STEP 4 - end results
 print()
-print(initial_directory_list)
+print(initial_directory_list)    #verifying all the files from the initial directory have been viewed
 print()
-print(image_types)
+print(new_directory)    #for reference
