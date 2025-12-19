872fe4d9ddfcca66e97d4605c6681811d3efb809
Nathaniel Olguin <business.nateolguin@gmail.com>
2025-12-19 10:28:04 -0600

    1st Update
    1st update to the analysis.py file
    
    COMPLETED:
       -   got the basic variables down for locating the 'data' directory
       -   started a base starting code to STEP 1
       -   analysis.py can read the 'data' directory
    
    
    NEXT STEPS:
       -   improve STEP 1 | reading the files. I am thinking, if i use the 'datetime' module to get the current date just like journal.py does I can specifically target the current days data entry that way instead of relying on the current if-else block to determine which file to read. 
       -   implement word count / character count

--- Journal AI Project/Analysis.py
+++ Journal AI Project/Analysis.py
@@ -1,7 +1,30 @@
-#⭐️ analysis.py
+#⭐️ analysis.py ⭐
 
 #Handles:
 #    reading the text
 #    analyzing it
 #    returning results
 #    saving a report into reports
+
+
+
+#IMPORTS
+from datetime import datetime    #not currently in use
+
+
+#VARIABLES
+initial_directory = os.getcwd()    #reference
+data_dir = f'{initial_directory}\\Data'    #directory that holds the files analysis.py will read
+data_dir_list = os.listdir(data_dir)
+
+
+#STEP 1  |  Read the latest journal 'Data' entry
+print(data_dir_list)    #reference
+if len(data_dir_list) < 2:    #if the 'Data' directory only has 1 entry read that entry
+    with open(f'{data_dir}\\{data_dir_list[0]}', 'r') as data:
+        content = data.read()
+        print(content)
+else:    #if the 'Data' directory only has more than 1 entry read the latest(last) entry
+    with open(f'{data_dir}\\{data_dir_list[-1]}', 'r') as data:
+        content = data.read()
+        print(content)
