885ba7670683cc100110edd48a7899af9b2f77a0
Nathaniel Olguin <business.nateolguin@gmail.com>
2026-01-15 14:10:00 -0600

    5th Update
    UPDATES:
       -   due to improvements on my internal module, njro.py, I was able to clean up the code of this file, analysis.py, and reduce the lines of code while also removing some hard-coded print functions. This change is to reflect my future goals to implement a GUI.

--- Journal AI Project/Analysis.py
+++ Journal AI Project/Analysis.py
@@ -25,25 +25,19 @@ data_dir_list = os.listdir(data_dir)
 todays_entry = f'Daily Journal  -  {simple_date}.md'
 
 if todays_entry in data_dir_list:    #program looks for todays journal entry
-    print('File Exists:  True')
     with open(f'{data_dir}\\{todays_entry}', 'r') as data:
         content = data.read()
 else:
-    print('File Exists:  False')
+    pass
 print(content)    #Dev Reference
 
 
 #STEP 2  |  Anaylize the 'content'
 njro.line()    #DEV READABILITY
-print(njro.character_count(content))
-print(njro.word_count(content))
 
-listed_words = content.split()    #turns the content into a list
-revised_words = []    #empty list for the end result of the njro.punctuation_checker function
-while len(listed_words) > 0:
-    word = listed_words.pop()
-    revised_words.append(njro.punctuation_checker(word))    #once words are fixed by the function, add to our original empty list
-    
-revised_words_str = ' '.join(revised_words)    #word_frequency requires a string argument
-print(njro.word_frequency(revised_words_str))
-njro.line()    #DEV READABILITY
+character_count = njro.character_count(content)
+word_count = njro.word_count(content)
+revised_content = njro.punctuation_checker(content)[1]    #returns a list of cleaned up words
+frequency = njro.word_frequency(revised_content)    #how often a word is used
+main, filler = njro.top_words(revised_content)    #top 3 most used main & filler words
+sentiment = njro.sentiment(revised_content)    #emotional tone of the text
