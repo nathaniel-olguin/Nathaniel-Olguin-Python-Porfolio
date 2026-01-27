b4cc6c257004a386098f64f2e70f97745fdb1a2f
Nathaniel Olguin <business.nateolguin@gmail.com>
2026-01-27 01:19:13 -0600

    Update: 0.2
    UPDATES:
       -   update_inventory()   |    new function that is supposed to update existing inventory items by identifying them with their ID number (Currently bugged: returning internal runtime failure 500)
       -   add_inventory()   |   new function that adds new items to the existing inventory
       -   remove_inventory()   |   new function that deletes existing items from the inventory list by identifying them with their ID number
       -   now utilizing pydantic module to make the new functions user-friendly
       -   inventory_items()   |   new class that defines the structure of items in the inventory list. 
       -   Learned about the helpful enumerate() function and updated my notes to reflect this
    
    NEXT STEPS:
       -   fix the exception for the update_inventory() function
    
    NOTES:
       -   I am having a lot of fun with this running servers and all that. Super cool stuff to see.
       -   I think the error I am running into with the update_inventory() function has to do with my inventory_items() class

--- Inventory-tasks App/backend/main.py
+++ Inventory-tasks App/backend/main.py
@@ -1,23 +1,51 @@
+#IMPORTS
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 #VARIABLES
-app = FastAPI()
+app = FastAPI()    #app server object
 inventory = [
     {'id':1000, 'name':'Bar Soap', 'quantity':3},
     {'id':1001, 'name':'Shampoo', 'quantity':1},
     {'id':1002, 'name':'Conditioner', 'quantity':1},
-    {'id':1003, 'name':'Face Wash', 'quantity':2}
 ]
 
 
-#FUNCTIONS
-@app.get('/')    #looking for requests
+#pydantic   |   FUNCTIONS
+class inventory_items(BaseModel):    #structure for inventory
+    id: int
+    name: str
+    quantity: int
+
+
+#FastAPI   |   FUNCTIONS
+@app.get('/')    #read status requests
 def api_status():
     return {'STATUS':'***SUCCESSFUL***'}
-
 api_status()
 
-@app.get('/inventory')    #looking for inventory requests
+@app.get('/inventory')    #read inventory requests
 def check_inventory():
     return inventory
+
+@app.post('/inventory')    #add inventory items
+def add_inventory(item: inventory_items):    
+    inventory.append(item.dict())    #adding to the end of the inventory dictionary
+    return item
+
+@app.put('/inventory/{id_number}')    #update existing inventory items    ***CURRENTLY CAUSING internal runtime error 500***
+def update_inventory(id_number: int, update: inventory_items):
+    for index, item in enumerate(inventory):    #numbered list of invenotry
+        if item['id'] == id_number:    #if 'item' (the id) in the 'index, item' pair of inventory is matches an existing 'id'
+            inventory[index] = updated.dict()
+            return updated
+    return {'***ERROR***': 'Item not found'}    #if id does not match display error message
+
+@app.delete('/inventory/{id_number}')    #delete inventory if its id_number exists
+def remove_inventory(id_number: int):
+    for item in inventory:
+        if item['id'] == id_number:
+            inventory.remove(item)
+            return {'***UPDATE***': 'Item Deleted from inventory'}
+    return {'***ERROR***': 'Item not found'}
