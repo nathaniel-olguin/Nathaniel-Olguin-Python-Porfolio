a5acfb2854e2cf91b54612aeb027a54039d7da25
Nathaniel Olguin <business.nateolguin@gmail.com>
2026-02-04 13:03:34 -0600

    Update: 0.4
    UPDATES:
       -   add_inventory()   |   Solve the layout of added items by recreating the base Inventory_Items() class. 
       -   Fixed the text formatting of my classes (according to the laws of python for better readability)
    
    NEXT STEPS:
       -   Begin working towards setting up SQL (studying how to set this up)

--- Inventory-tasks App/backend/main.py
+++ Inventory-tasks App/backend/main.py
@@ -14,13 +14,17 @@ inventory = [
 
 
 #pydantic   |   CLASSES
-class inventory_items_body(BaseModel):    #body of the inventory structure (excluding ID) ***MUTABLE***
+class Inventory_Items_Body(BaseModel):    #body of the inventory structure (excluding ID) ***MUTABLE***
     name: str
     quantity: int
 
-class inventory_items_id(inventory_items_body):    #full object with ID
+class Inventory_Items_Id(Inventory_Items_Body):    #full object with ID
     id: int
 
+class Inventory_Items(BaseModel):    #base outline blueprint of the inventory items 
+    id: int
+    name: str
+    quantity: int
 
 
 #FastAPI   |   FUNCTIONS
@@ -34,12 +38,12 @@ def check_inventory():
     return inventory
 
 @app.post('/inventory')    #add inventory items
-def add_inventory(item: inventory_items_id):    
+def add_inventory(item: Inventory_Items):    
     inventory.append(item.dict())    #adding to the end of the inventory list of dictionaries
     return item
 
 @app.put('/inventory/{id_number}')    #update existing inventory items
-def update_inventory(id_number: int, update: inventory_items_body):    #uses the body class so that the ID is not affected by the update***
+def update_inventory(id_number: int, update: Inventory_Items_Body):    #uses the body class so that the ID is not affected by the update***
     for index, item in enumerate(inventory):    #numbered list of invenotry
         if item['id'] == id_number:    #if 'item' (the id) in the 'index, item' pair of inventory is matches an existing 'id'
             #UPDATE name/quantity without altering the ID#
@@ -50,7 +54,7 @@ def update_inventory(id_number: int, update: inventory_items_body):    #uses the
         
     return {'***ERROR***': 'Item not found'}    #if id does not match display error message
 
-@app.delete('/inventory/{id_number}')    #delete inventory if its id_number exists
+@app.delete('/inventory/{id_number}')    #delete inventory if its id_number exists   (id_number ⇶ URL)
 def remove_inventory(id_number: int):
     for item in inventory:
         if item['id'] == id_number:
